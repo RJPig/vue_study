ff85fa80e0684d7b74cfc09a9dfa3bc4c7c0001f
RJPig <1259950227@qq.com>
2020-06-05 22:51:05 +0800

    no message

--- tabbar/src/components/maintabbar/MainTabBar.vue
+++ tabbar/src/components/maintabbar/MainTabBar.vue
@@ -1,21 +1,21 @@
 <template>
   <tab-bar>
-    <tab-bar-item path="/home" activeColor="blue">
+    <tab-bar-item path="/home" activeColor="deeppink">
       <img slot="item-icon" src="" alt="">
       <img slot="item-icon-active" src="" alt="">
       <div slot="item-text">首页</div>
     </tab-bar-item>
-    <tab-bar-item path="/category" activeColor="blue">
+    <tab-bar-item path="/category" activeColor="deeppink">
       <img slot="item-icon" src="" alt="">
       <img slot="item-icon-active" src="" alt="">
       <div slot="item-text">分类</div>
     </tab-bar-item>
-    <tab-bar-item path="/shopcart" activeColor="blue">
+    <tab-bar-item path="/shopcart" activeColor="deeppink">
       <img slot="item-icon" src="" alt="">
       <img slot="item-icon-active" src="" alt="">
       <div slot="item-text">购物车</div>
     </tab-bar-item>
-    <tab-bar-item path="/profile" activeColor="blue">
+    <tab-bar-item path="/profile" activeColor="deeppink">
       <img slot="item-icon" src="" alt="">
       <img slot="item-icon-active" src="" alt="">
       <div slot="item-text">我的</div>
